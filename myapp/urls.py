414fe0b268bdb84d0a4dad1ebf7de2018e2d993c
maheshnikange <maheshnikange7@gmail.com>
2024-12-29 19:53:02 +0530

    postman

--- myapp/urls.py
+++ myapp/urls.py
@@ -33,4 +33,11 @@ urlpatterns = [
     path("customers/", views.customer_list, name="customer_list"),
     path("customers/create/", views.customer_create, name="customer_create"),
     path("customers/<int:pk>/", views.customer_detail, name="customer_detail"),
+
+    path('projects/', views.project_list, name='project_list'),
+    path('projects/create/', views.project_create, name='project_create'),
+    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('projects/<int:project_id>/update/', views.project_update, name='project_update'),
+    path('projects/<int:project_id>/delete/', views.project_delete, name='project_delete'),
+
 ]
